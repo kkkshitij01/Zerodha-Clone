b8ecfc2be6d5e210b1e5186b60fc4e62ef13f5b7
Kshitij Gupta <gkshitij0510@gmail.com>
2025-10-07 23:36:39 +0530

    Project Setup Done Here

--- frontend/src/Landing-page/Nav.jsx
+++ frontend/src/Landing-page/Nav.jsx
@@ -1,5 +1,12 @@
 export default function Nav() {
     return (
-        <h1>Nav</h1>
-    )
+        <>
+            <h1>Nav</h1>
+            <i className="fa fa-home"></i>        {/* Home */}
+            <i className="fa fa-envelope"></i>    {/* Envelope / Email */}
+            <i className="fa fa-user"></i>        {/* User */}
+            <i className="fa fa-star"></i>        {/* Star */}
+            <i className="fa fa-github"></i>      {/* GitHub brand */}
+        </>
+    );
 }
