6d8658f05ae961dfa26b618804e5c0c92b60f00c
Kshitij Gupta <gkshitij0510@gmail.com>
2025-10-10 22:19:46 +0530

    completed The full Home page

--- frontend/src/Landing-page/Footer.jsx
+++ frontend/src/Landing-page/Footer.jsx
@@ -1,41 +1,91 @@
 export default function Footer() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <img src="media/images/logo.svg" alt="Logo" style={{ width: "50%" }} />
-                    <p>© 2010 - 2025, Zerodha Broking Ltd.
+        <footer>
 
-                        All rights reserved.</p>
-                </div>
-                <div className="col">
-                    <p>Company</p>
-                    <a href="">About</a><br />
-                    <a href="">Product</a><br />
-                    <a href="">Pricing</a><br />
-                    <a href="">Referral Program</a><br />
-                    <a href="">Careers</a><br />
-                    <a href="">Zerodha.tech</a><br />
-                    <a href="">Press & Media</a><br />
-                    <a href="">Zerodha Cares</a><br />
-                </div>
-                <div className="col">
-                    <p>Support </p>
-                    <a href="">Contact</a><br />
-                    <a href="">Support Portal</a><br />
-                    <a href="">Z-Connect Blog</a><br />
-                    <a href="">List of charges </a><br />
-                    <a href="">Resources</a><br />
+            <div className="container border-top ">
+                <div className="row">
+                    <div className="col mt-5 " style={{ marginLeft: "50px" }}>
+                        <img src="media/images/logo.svg" alt="Logo" style={{ width: "50%" }} />
+                        <p className="text-muted mt-4" style={{ fontSize: "14px" }}>© 2010 - 2025, Zerodha Broking Ltd.
 
-                </div>
-                <div className="col">
-                    <p>Account</p>
-                    <a href="">Open an account</a><br />
-                    <a href="">Fund transfer</a><br />
-                    <a href="">60 days challange</a><br />
+                            All rights reserved.</p>
+                    </div>
+                    <div className="col mt-5 d-flex flex-column justify-content-center align-items-start" style={{ gap: "10px" }}>
+                        <p>Company</p>
+                        <a href="">About</a>
+                        <a href="">Product</a>
+                        <a href="">Pricing</a>
+                        <a href="">Referral Program</a>
+                        <a href="">Careers</a>
+                        <a href="">Zerodha.tech</a>
+                        <a href="">Press & Media</a>
+                        <a href="">Zerodha Cares</a>
+                    </div>
+                    <div className="col mt-5 d-flex flex-column justify-content-start align-items-start" style={{ gap: "10px" }}>
+                        <p>Support </p>
+                        <a href="">Contact</a>
+                        <a href="">Support Portal</a>
+                        <a href="">Z-Connect Blog</a>
+                        <a href="">List of charges </a>
+                        <a href="">Resources</a>
 
+                    </div>
+                    <div className="col mt-5 d-flex flex-column justify-content-start align-items-start" style={{ gap: "10px" }}>
+                        <p>Account</p>
+                        <a href="">Open an account</a>
+                        <a href="">Fund transfer</a>
+                        <a href="">60 days challange</a>
+
+                    </div>
+                </div >
+                <div className="text-muted p-5" style={{ fontSize: "11px", color: "#9B9B9B !important" }}>
+
+                    <p>
+                        Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025; SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF
+                    </p>
+
+
+
+                    <p>
+                        Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances
+
+                    </p>
+                    <p>
+                        Smart Online Dispute Resolution | Grievances Redressal Mechanism
+                    </p>
+                    <p>
+
+                        Investments in securities market are subject to market risks; read all the related documents carefully before investing.
+
+                    </p>
+                    <p>
+
+                        Attention investors: 1 Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2 Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3 Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.
+
+                    </p>
+                    <p>
+                        India's largest broker based on networth as per NSE. NSE broker factsheet
+                    </p>
+
+                    <p>
+                        "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.
+                    </p>
+                </div >
+                <div className="row">
+                    <div className="col d-flex  justify-content-center align-items-start" style={{ gap: "40px", marginTop: "-50px" }}>
+
+                        <a href="">NSE</a>
+                        <a href="">BSE</a>
+                        <a href="">MCX </a>
+                        <a href="">Terms & conditions  </a>
+                        <a href="">Policies & procedures  </a>
+                        <a href="">Privacy policy  </a>
+                        <a href="">Disclosure  </a>
+                        <a href="">Investor charter</a>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </div >
+
+        </footer >
     )
 }
